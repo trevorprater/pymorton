a732e5b72d804cfb157a2ad72908d75e9a97b6f9
R Trevor Prater <richard.prater@bnymellon.com>
2018-02-14 16:51:56 -0500

    Adds long_description for PyPi

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 def build():
     setup(
-            name = 'pymorton',
-            version = '0.2.2',
-            author = 'Trevor Prater',
-            author_email = 'trevor.prater@gmail.com',
-            description = 'A lightweight morton coder with lat/long support.',
-            license = 'MIT',
-            keywords = 'nearest neighbors, geo hashing, geo, z-order, morton coding, hashing',
-            url = 'https://github.com/trevorprater/pymorton',
-            packages = ['pymorton'],
+            name='pymorton',
+            version='0.2.3',
+            author='Trevor Prater',
+            author_email='trevor.prater@gmail.com',
+            description='A lightweight morton coder with lat/long support.',
+            long_description=long_description,
+            license='MIT',
+            keywords='nearest neighbors, geo hashing, geo, z-order, morton coding, hashing',
+            url='https://github.com/trevorprater/pymorton',
+            packages=['pymorton'],
             python_requires='>=2.6',
-            install_requires = [],
-            classifiers = [
+            install_requires=[],
+            classifiers=[
                 'Development Status :: 5 - Production/Stable',
                 'Topic :: Utilities',
                 'License :: OSI Approved :: MIT License'
