f5adc11ca0362cae064cdb2c214a46a959e81a0a
R Trevor Prater <richard.prater@bnymellon.com>
2018-02-14 21:06:19 -0500

    Replaces DESCRIPTION.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import setup
 
-with open('DESCRIPTION.txt') as f:
-    long_description = f.read()
+LONG_DESCRIPTION = """
+Ordinal hashing of multidimensonal data and geographic coordinates via Morton coding / Z-ordering.
+
+In mathematical analysis and computer science, `Z-order`, `Morton-order`, or a `Morton-code` is a function
+which maps multidimensional data to one dimension while preserving locality of the data points.
+It was introduced in 1966 by IBM researcher, G. M. Morton. The z-value of a point in multidimensions is
+calculated by interleaving the binary representations of its coordinate values. Once the data are sorted
+into this ordering, any one-dimensional data structure can be used, such as binary search trees, B-trees,
+skip lists, or hash tables. The resulting ordering can equivalently be described as the order one would
+achieve from a depth-first traversal of a quadtree, where `{x, y, ..., K}` are combined into a single
+ordinal value that is easily compared, searched, and indexed against other Morton numbers.
+"""
+
 
 def build():
     setup(
             name='pymorton',
-            version='1.0.4',
+            version='1.0.5',
             author='Trevor Prater',
             author_email='trevor.prater@gmail.com',
             description='A lightweight morton coder with lat/long support.',
-            long_description=long_description,
+            long_description=LONG_DESCRIPTION,
             license='MIT',
             keywords='nearest neighbors, geo hashing, geo, z-order, morton coding, hashing',
             url='https://github.com/trevorprater/pymorton',
